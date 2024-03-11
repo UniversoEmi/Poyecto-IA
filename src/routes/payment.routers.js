0ab6fd3475c72150999627e8ede487491fb6be29
universoemi <universoemi>
2024-03-11 20:03:32 -0300

    TerceraVersion

--- src/routes/payment.routers.js
+++ src/routes/payment.routers.js
@@ -1,11 +1,12 @@
-import {Router} from 'express'
+import {Router} from 'express';
+import {createOrder} from '../routes/controllers/paymetn.controller.js';
 
-const router = Router()
+const router = Router();
 
-router.get('/create.order', (req, res) => res.send('creating oreden'))
+router.get('/create-order', createOrder); //http://localhost:4000/create-order
 
-router.get('/success', (req, res) => res.send('creating oreden'))
+router.get('/success', (req, res) => res.send('creating order'));
 
-router.get('/webhook', (req, res) => res.send('creating oreden'))
+router.get('/webhook', (req, res) => res.send('creating order'));
 
-export default router
+export default router;
