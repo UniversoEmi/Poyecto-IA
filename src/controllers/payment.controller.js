f607f7489ec718b1e6e7b00142360ef09bac8301
universoemi <universoemi>
2024-03-14 21:10:41 -0300

    arreglando

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,1 +1,26 @@
-export const createOrder = (req, res) => res.send ('creating order')
+import mercadopago from "mercadopago";
+
+export const createOrder = async (req, res) => {
+    
+    mercadopago.configure({
+        access_token: 
+        'EST-1803343693495513-031302-c1900d6daa9003f2b3c54e32a6c102c5-1726167154',
+    });
+    
+
+const result = await mercadopago.preferences.create({
+    items: [
+        {
+            title: "Laptop",
+            unit_price: 12000,
+            currency_id: "ARS",
+            quantity: 1,
+
+        }]
+
+})
+
+console.log(result)
+
+res.send("crando orden");
+};
